1bd38634f63ce4da30f19ceefb310f81ea7232b8
Isakiye Afasha <afashaisakiye@gmail.com>
2021-01-26 10:53:26 +0300

    patch

--- api/hubmananger/suppliers/views.py
+++ api/hubmananger/suppliers/views.py
@@ -20,3 +20,4 @@ class SupplierDetailViewSet(generics.RetrieveUpdateDestroyAPIView , IsHubManange
     def get_queryset(self):
         return Supplier.objects.all()
     
+class SupplierReceiversViewSet(generics.ListCreateAPIView )
